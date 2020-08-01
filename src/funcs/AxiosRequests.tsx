c853d6c20c791167cba00a7a37ffd2878e401775
Siobhan McKenzie <siobhan@Siobhans-MacBook-Pro.local>
2020-08-01 21:20:10 +0100

    first commit

--- src/funcs/AxiosRequests.tsx
+++ src/funcs/AxiosRequests.tsx
@@ -1,11 +1,9 @@
 import axios from 'axios'
 
-export default function AxiosRequests(
-  userInput: string = 'happy happy sad joy sad'
-): any {
+export default function AxiosRequests(userInput: string): any {
   axios
     .post('http://localhost:5000/api/v1/wordfilter', {
-      name: 'frontend test happy request',
+      name: 'happyOrSad request',
       description: userInput
     })
     .then(response => {
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,56 @@
 import axios from 'axios'
 import React from 'react'
-
-import AxiosRequests from '../funcs/AxiosRequests'
+import { string } from 'prop-types'
 
 type MyProps = {}
-type MyState = { formValue: string; debugging: string }
+type MyState = { formValue: string; happyOrSad: any; numericalValue: number }
 
 class HomePage extends React.Component<MyProps, MyState> {
   constructor(props: any) {
     super(props)
     this.state = {
       formValue: '',
-      debugging: 'COMPONENT DID MOUNT'
+      happyOrSad: null,
+      numericalValue: 0
     }
   }
 
   handleChange = (event: any) => {
-    // console.log('DEBUGGING' + event.target.value)
     this.setState({ formValue: event.target.value })
   }
 
   handleSubmit(event: any) {
-    console.log('A name was submitted: ' + this.state.formValue)
-    // AxiosRequests(this.state.formValue)
+    console.log('The rocket launcher was submitted: ' + this.state.formValue)
+    const axiosValue = axios
+      .post('http://localhost:5000/api/v1/wordfilter', {
+        name: 'happyOrSad request',
+        description: this.state.formValue
+      })
+      .then(response => {
+        console.log('Post successful!')
+        console.log(response)
+        return response
+      })
+      .catch(error => {
+        //   if (error.response.status === 409) {
+        //     alert('throwing error exists!')
+        //   } else {
+        //     alert('Unknown error.')
+        //   }
+        return error
+      })
+
+    console.log('NEW DEBUGGING' + axiosValue)
+
+    // this.setState({happyOrSad: axiosValue.returnedValue,
+    //     numericalValue: 0})
+
     event.preventDefault()
   }
 
   componentDidMount() {
-    console.log(this.state.debugging)
-    AxiosRequests('I am so sad')
+    // console.log(this.state.debugging)
+    // AxiosRequests('I am so sad')
   }
 
   render() {
@@ -44,7 +66,11 @@ class HomePage extends React.Component<MyProps, MyState> {
               onChange={this.handleChange}
             />
           </label>
-          <input type='submit' value='Submit' />
+          <input
+            type='submit'
+            value='Submit'
+            disabled={this.state.formValue.length < 1}
+          />
         </form>
       </div>
     )
