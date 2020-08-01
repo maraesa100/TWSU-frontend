65401a9b8241ddc1cff4df3cf70ad415c952fb59
Siobhan McKenzie <siobhan@Siobhans-MacBook-Pro.local>
2020-08-01 19:33:32 +0100

    stable frontend - endpoints working

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -12,6 +12,23 @@ class MyComponent extends React.Component {
     axios.get('http://localhost:5000/').then(response => {
       console.log(response.data)
     })
+
+    axios
+      .post('http://localhost:5000/api/v1/wordfilter', {
+        name: 'frontend test happy request',
+        description: 'happy happy sad joy sad'
+      })
+      .then(response => {
+        console.log('Post successful!')
+        console.log(response)
+      })
+      .catch(error => {
+        if (error.response.status === 409) {
+          alert('Doggie with same name exists!')
+        } else {
+          alert('Unknown error.')
+        }
+      })
   }
   render() {
     return <h1>my Component has Mounted, Check the browser 'console' </h1>
