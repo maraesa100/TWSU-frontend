6949647276b046ce9b996da6595aded5e4aa118c
Siobhan McKenzie <siobhan@Siobhans-MacBook-Pro.local>
2020-08-04 15:19:11 +0100

    Updates

---
+++ precache-manifest.7b02ec6a157e639f67e82ed5f355dfaf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639775ef17d6bdb28da21a11cacd2616",
+    "url": "/TWSU-frontend/index.html"
+  },
+  {
+    "revision": "ac169f8796ff788d2fe6",
+    "url": "/TWSU-frontend/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d5ce3b1e2b4caf9cf156",
+    "url": "/TWSU-frontend/static/css/main.e2e0483d.chunk.css"
+  },
+  {
+    "revision": "ac169f8796ff788d2fe6",
+    "url": "/TWSU-frontend/static/js/2.c0672b33.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/TWSU-frontend/static/js/2.c0672b33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ce3b1e2b4caf9cf156",
+    "url": "/TWSU-frontend/static/js/main.a3deb3db.chunk.js"
+  },
+  {
+    "revision": "85facda80b97d04eb925",
+    "url": "/TWSU-frontend/static/js/runtime-main.4caba9ba.js"
+  },
+  {
+    "revision": "4ddbfee05e36797e27622f4f97f73532",
+    "url": "/TWSU-frontend/static/media/happy.4ddbfee0.png"
+  },
+  {
+    "revision": "61cbc002cffc508710f9a01d28e8a3a5",
+    "url": "/TWSU-frontend/static/media/rocket-launch.61cbc002.gif"
+  },
+  {
+    "revision": "6abcf3ec05b9a78ca85d1344eece5ecf",
+    "url": "/TWSU-frontend/static/media/rocket-launch.6abcf3ec.jpg"
+  },
+  {
+    "revision": "8944e8856ddab3622f915da52d678f00",
+    "url": "/TWSU-frontend/static/media/sad.8944e885.png"
+  }
+]);
